d4332ca7a60ed75ac7de8a2445bbfb0cdb6d2c87
Karen Spriggs <k.spriggs01@gmail.com>
2022-04-02 19:55:56 -0500

    they can move now

--- artifactReleases/1POC/MonsterWorld/Assets/Scripts/Monster/Monster.cs
+++ artifactReleases/1POC/MonsterWorld/Assets/Scripts/Monster/Monster.cs
@@ -5,10 +5,28 @@ using UnityEngine;
 public class Monster : MonoBehaviour
 {
     public BattleStats battleStats;
+    public MonsterMovement monMonvement;
+
+    public float speed;
+    public int max;
 
     public Monster()
     {
         battleStats = new BattleStats();
+    }
+
+    private void Start()
+    {
+        monMonvement = new MonsterMovement();
+        monMonvement.position = this.transform.position;
+        monMonvement.Speed = speed;
+        monMonvement.max = max;
+    }
+
+    private void Update()
+    {
+        monMonvement.DetermineState();
+        this.transform.position = monMonvement.position;
     }
 
 }
